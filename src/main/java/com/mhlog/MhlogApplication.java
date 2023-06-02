208bb9bb04919c05880606644fa22c54387c4598
MHLEE12 <coolmh12@gmail.com>
2023-06-02 15:08:13 +0900

    Init: blog project 생성

---
+++ src/main/java/com/mhlog/MhlogApplication.java
@@ -0,0 +1,13 @@
+package com.mhlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhlog/MhlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
