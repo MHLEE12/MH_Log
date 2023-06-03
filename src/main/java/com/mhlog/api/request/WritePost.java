6fc8c44a2a37b16cd0bd491bfd4a2cf3c1ece702
MHLEE12 <coolmh12@gmail.com>
2023-06-03 22:41:25 +0900

    Feat: post관련 Controller와 DTO 생성

---
+++ src/main/java/com/mhlog/api/request/WritePost.java
@@ -0,0 +1,29 @@
+package com.mhlog.api.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+
+@ToString
+@Setter
+@Getter
+public class WritePost {
+
+    @NotBlank(message = "제목을 입력해주세요.")
+    private String title;
+
+    @NotBlank(message = "내용을 입력해주세요.")
+    private String content;
+
+    @Builder
+    private WritePost(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public WritePost() {
+    }
+}
