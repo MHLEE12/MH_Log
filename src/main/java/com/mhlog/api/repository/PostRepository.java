caafa0762edb26259063d04d3a94e0f68add1982
MHLEE12 <coolmh12@gmail.com>
2023-06-04 13:45:43 +0900

    Feat: PostService, PostRepository 생성

---
+++ src/main/java/com/mhlog/api/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.mhlog.api.repository;
+
+import com.mhlog.api.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+
+}
