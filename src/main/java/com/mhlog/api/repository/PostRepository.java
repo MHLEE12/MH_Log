eaf0722ccb688be45a9d160050c5a91acbddf1a6
MHLEE12 <coolmh12@gmail.com>
2023-06-09 22:18:05 +0900

    Feat: 페이징 처리를 위한 PostRepository 추가2

--- src/main/java/com/mhlog/api/repository/PostRepository.java
+++ src/main/java/com/mhlog/api/repository/PostRepository.java
@@ -3,7 +3,7 @@ package com.mhlog.api.repository;
 import com.mhlog.api.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
 
 }
