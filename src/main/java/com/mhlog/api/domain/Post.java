0c34ac6fbf5ae09e9f0534a01ddef84f9c468ced
MHLEE12 <coolmh12@gmail.com>
2023-06-11 19:46:20 +0900

    Feat: Post클래스에 post 수정을 위한 메소드 추가

--- src/main/java/com/mhlog/api/domain/Post.java
+++ src/main/java/com/mhlog/api/domain/Post.java
@@ -27,9 +27,14 @@ public class Post {
         this.content = content;
     }
 
-    public void change(String title, String content) {
-        this.title = title;
-        this.content = content;
+    public PostEditor.PostEditorBuilder toEditor() {
+        return PostEditor.builder()
+                .title(title)
+                .content(content);
     }
 
+    public void edit(PostEditor postEditor) {
+        title = postEditor.getTitle();
+        content = postEditor.getContent();
+    }
 }
