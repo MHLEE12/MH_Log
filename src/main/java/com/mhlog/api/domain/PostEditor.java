24b7007c0cfd940fcb3fd1d9c61a796371aecfcb
MHLEE12 <coolmh12@gmail.com>
2023-06-11 19:44:58 +0900

    Feat: post 수정시 사용할 DTO 추가

---
+++ src/main/java/com/mhlog/api/domain/PostEditor.java
@@ -0,0 +1,19 @@
+package com.mhlog.api.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostEditor  {
+
+    private final String title;
+    private final String content;
+
+    @Builder
+    public PostEditor(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+}
